e078c68a6e06c69809bb3ec79a6dd48746fd6fb4
Gal Koaz <76553647+GalKoaz@users.noreply.github.com>
2021-10-30 14:47:32 +0300

    update NumClass.h

---
+++ NumClass.h
@@ -0,0 +1,15 @@
+//
+// Created by Gal on 29/10/2021.
+//
+
+#ifndef UNTITLED2_NUMCLASS_H
+#define UNTITLED2_NUMCLASS_H
+
+
+int isPrime(int num);
+int isStrong(int num);
+int isPalindrome(int num);
+int isArmstrong(int num);
+
+
+#endif //UNTITLED2_NUMCLASS_H
