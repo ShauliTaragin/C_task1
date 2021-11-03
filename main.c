92d961377cc7ed04beef01150aa67e2b3965f892
Shauli Taragin <shaulitaragin@gmail.com>
2021-11-03 16:07:04 +0200

    ori

--- main.c
+++ main.c
@@ -3,32 +3,33 @@
 
 int main() {
     int num1, num2;
-    printf("Please enter two non negative numbers : ");
+    printf("Please enter two non negative numbers:");
     scanf("%d %d", &num1, &num2);
 
-    printf("The Armstrong numbers are: ");
+    printf("The Armstrong numbers are:");
     for (int i = num1; i <= num2; ++i) {
         if (isArmstrong(i)) {
-            printf("%d ", i);
+            printf(" %d", i);
         }
     }
-    printf("\nThe Palindromes are: ");
+    printf("\nThe Palindromes are:");
     for (int i = num1; i <= num2; ++i) {
         if (isPalindrome(i)) {
-            printf("%d ", i);
+            printf(" %d", i);
         }
     }
-    printf("\nThe Prime numbers are: ");
+    printf("\nThe Prime numbers are:");
     for (int i = num1; i <= num2; ++i) {
         if (isPrime(i)) {
-            printf("%d ", i);
+            printf(" %d", i);
         }
     }
-    printf("\nThe Strong numbers are: ");
+    printf("\nThe Strong numbers are:");
     for (int i = num1; i <= num2; ++i) {
         if (isStrong(i)) {
-            printf("%d ", i);
+            printf(" %d", i);
         }
     }
+    printf("\n");
     return 0;
 }
